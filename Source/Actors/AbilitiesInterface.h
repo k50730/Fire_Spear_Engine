eb44d7489cecaf6a328e0fec3aaa08245403a123
HsiaoTe Tang <k50730@hotmail.com>
2019-09-27 16:45:33 -0400

    Actors folder added
    with Abilites interface class added

---
+++ Source/Actors/AbilitiesInterface.h
@@ -0,0 +1,12 @@
+#pragma once
+
+class AbilitiesInterface 
+{
+public:
+
+protected:
+
+private:
+
+
+};
---
+++ Source/Actors/SpecialAbilityComponent.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "AbilitiesInterface.h"
+
+
+class SpecialAbilityComponent : public AbilitiesInterface
+{
+public:
+	SpecialAbilityComponent();
+protected:
+
+private:
+};
+
