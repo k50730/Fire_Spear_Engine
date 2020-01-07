622bc34072035e96087725403ce4e5ae78257b31
MichaelVu <TuanMinh.Vu@georgebrown.ca>
2020-01-07 16:31:03 -0500

    Integrate SFML

--- Source/FireSpearEngine/FireSpearEngine/main.cpp
+++ Source/FireSpearEngine/FireSpearEngine/main.cpp
@@ -1,11 +1,11 @@
-
+#include <SFML/Graphics.hpp>
 #include "CoreEngine.h"
 
 
 
 int main() 
 {
-	CoreEngine* engine = new CoreEngine();
+	/*CoreEngine* engine = new CoreEngine();
 
 	if (engine->InitilizeSystem())
 	{
@@ -14,5 +14,25 @@ int main()
 	}
 
 	delete engine;
-	return 0;
+	return 0;*/
+
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
 }
